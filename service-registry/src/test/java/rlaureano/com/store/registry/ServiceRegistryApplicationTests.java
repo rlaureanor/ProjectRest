ae3bc15a758a980a23ee50d11e0b540e4c12cf87
Rogelio Laureano <rlaureanor@gmail.com>
2020-05-27 21:49:15 -0500

    Regitry service for eurecka server

---
+++ service-registry/src/test/java/rlaureano/com/store/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package rlaureano.com.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
